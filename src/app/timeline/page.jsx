9ad057a888999b433f55a6f6db2f86434b35ae78
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-04-15 12:43:24 +0600

    search added

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -15,34 +15,63 @@ const TimeLinePage = () => {
       ? allEntries
       : allEntries.filter((entry) => entry.type === filter);
 
-  console.log(checkCall);
+      const [search, setSearch]=useState("")
+      const searchFilter=search===""? filtered : filtered.filter((entry)=>entry.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+
   return (
     <div className="mt-6 w-10/12 mx-auto mb-6">
       <h2 className="text-4xl font-bold mb-3">Timeline</h2>
 
-      <div className="dropdown mb-4">
-        <div tabIndex={0} role="button" className="btn m-1">
-          {filter} <IoIosArrowDown />
+      <div className="flex justify-between items-center">
+        <div className="dropdown mb-4">
+          <div tabIndex={0} role="button" className="btn m-1">
+            {filter} <IoIosArrowDown />
+          </div>
+          <ul
+            tabIndex="0"
+            className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm"
+          >
+            {["All", "Call", "Text", "Video"].map((type) => (
+              <li
+                key={type}
+                onClick={() => setFilter(type)}
+                className={filter === type ? "bg-[#244D3F] text-white" : ""}
+              >
+                <a>{type}</a>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul
-          tabIndex="0"
-          className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm"
-        >
-          {["All", "Call", "Text", "Video"].map((type) => (
-            <li key={type}  onClick={() => setFilter(type)}
-              className={filter === type ? "bg-[#244D3F] text-white" : ""}>
-             
-              <a>{type}</a>
-            </li>
-          ))}
-        </ul>
+
+        <div>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input onChange={(e)=>setSearch(e.target.value)}  type="search" required placeholder="Search" />
+          </label>
+        </div>
       </div>
 
       <div>
-        {filtered.length === 0 ? (
+        {searchFilter.length === 0 ? (
           <p className="text-[#64748B]">No interactions yet.</p>
         ) : (
-          filtered.map((entry, ind) => (
+          searchFilter.map((entry, ind) => (
             <TimeLineDisplay key={ind} entry={entry} />
           ))
         )}
